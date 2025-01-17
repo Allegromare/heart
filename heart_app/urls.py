155f200ef0468d90a64f2affc6a2363caa9cf2d0
Allegromare <Allegromare@gmail.com>
2025-01-17 22:43:50 +0100

    refactor: Remove duplicate urlpatterns and imports

--- heart_app/urls.py
+++ heart_app/urls.py
@@ -4,10 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('/about', views.about, name='about'),
-]
-from django.urls import path
-from . import views
-
-urlpatterns = [
     path('add/', views.add_reading, name='add_reading'),
 ]

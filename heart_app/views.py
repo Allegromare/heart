8845b9a6770315eb801c89cbf9bd4b0581f27a2d
Allegromare <Allegromare@gmail.com>
2025-01-26 21:31:05 +0100

    fix: Fix date parsing and add error message

--- heart_app/views.py
+++ heart_app/views.py
@@ -6,7 +6,8 @@ from django.contrib import messages
 
 def homepage(request):
     readings = HeartReading.objects.all().order_by('reading_date')
-    print("READINGS[1]:", readings[1].reading_date)
+    print("HOMEPAGE READINGS[1]:", readings[1].reading_date)
+
 
     return render(request, 'heart_app/homepage.html', {'readings': readings})
 
@@ -15,23 +16,20 @@ def about(request):
 
 def add_reading(request):
     now = timezone.localtime(timezone.now())
-    print(now)
+    print("ADD_READING - NOW: ", now)
     #context = {'default_date': now} #.strftime('%Y-%m-%d')}
 
     if request.method == 'POST':
         min_pressure = request.POST.get('min_pressure')
         max_pressure = request.POST.get('max_pressure')
         heart_rate = request.POST.get('heart_rate')
-        reading_date_str = request.POST.get('reading_date')
-        
-        print("NOW ", now)
-        print("READING DATE", reading_date_str)
-        try:
-            reading_date = datetime.datetime.fromisoformat(reading_date_str)
-        except ValueError:
-            messages.error(request, 'Invalid date or time format.')
-            return render(request, 'heart_app/add_reading.html', {'default_date': now})
 
+        try:
+            reading_date = datetime.datetime.fromisoformat(request.POST.get('reading_date'))
+        except ValueError:
+            messages.error(request, 'La data o l\'ora non sono valide')
+            return render(request, 'heart_app/add_reading.html', {'default_date':now})
+        
         if not all(x and 25 < int(x) < 250 for x in [min_pressure, max_pressure, heart_rate]):
             messages.error(request, 'I valori di pressione e battito cardiaco devono essere compresi tra 25 e 250.')
             return render(request, 'heart_app/add_reading.html')

c8b3cbc7eba00da809d2f9b3ff646080d0c6d4ad
Allegromare <Allegromare@gmail.com>
2025-01-26 21:12:30 +0100

    fix: Print reading date and form data for debugging

--- heart_app/views.py
+++ heart_app/views.py
@@ -6,6 +6,8 @@ from django.contrib import messages
 
 def homepage(request):
     readings = HeartReading.objects.all().order_by('reading_date')
+    print("READINGS[1]:", readings[1].reading_date)
+
     return render(request, 'heart_app/homepage.html', {'readings': readings})
 
 def about(request):
@@ -22,6 +24,9 @@ def add_reading(request):
         heart_rate = request.POST.get('heart_rate')
         reading_date = request.POST.get('reading_date')
         
+        print("NOW ", now)
+        print("READING DATE", reading_date)
+
         if not all(x and 25 < int(x) < 250 for x in [min_pressure, max_pressure, heart_rate]):
             messages.error(request, 'I valori di pressione e battito cardiaco devono essere compresi tra 25 e 250.')
             return render(request, 'heart_app/add_reading.html')
